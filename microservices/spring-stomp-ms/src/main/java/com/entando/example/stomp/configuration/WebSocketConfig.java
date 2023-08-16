cce7b68eacf50ebcc0f328472d66754f394e8e5a
Nathan Shaw <n.shaw@entando.com>
2023-08-16 12:56:20 -0400

    Finish service with cors controls to allow local html file for testing

---
+++ microservices/spring-stomp-ms/src/main/java/com/entando/example/stomp/configuration/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.entando.example.stomp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+         registry.addEndpoint("/chat")
+                //NOTE: this is not recommended for production use and should be replaced by dev/prod profiles
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS();
+    }
+}
---
+++ microservices/spring-stomp-ms/src/main/java/com/entando/example/stomp/entity/Message.java
@@ -0,0 +1,15 @@
+package com.entando.example.stomp.entity;
+
+public class Message {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
