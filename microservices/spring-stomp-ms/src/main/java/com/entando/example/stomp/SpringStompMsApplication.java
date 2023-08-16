e9bddcc1dea0e7d07b15d9df588a4ea08356c6ab
Nathan Shaw <n.shaw@entando.com>
2023-08-16 10:17:49 -0400

    Initialize bundle and add starter spring app

---
+++ microservices/spring-stomp-ms/src/main/java/com/entando/example/stomp/SpringStompMsApplication.java
@@ -0,0 +1,13 @@
+package com.entando.example.stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStompMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStompMsApplication.class, args);
+	}
+
+}
---
+++ microservices/spring-stomp-ms/src/test/java/com/entando/example/stomp/SpringStompMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entando.example.stomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStompMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
